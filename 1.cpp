eadf0d1286c5aac202f9ed16cc3c775ffe421030
Vu <115148621+truongvanvu21@users.noreply.github.com>
2023-05-17 21:05:02 +0700

    Danh_Sach_Lien_Ket_Don_I

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,3 @@
-// quan li sinh vien
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -17,127 +16,146 @@ node makeNode(int x){
 }
 int Size(node a){
 	int cnt=0;
-	while(a != NULL){
+	while(a != NULL){	
 		cnt++;
 		a=a->next;
 	}
-	return cnt;
+	return cnt;	
 }
-void InsertFirst(node &a, int x){
+//chen dau
+void InsertHead	(node &a, int x){
 	node tmp=makeNode(x);
 	if(a==NULL){
 		a=tmp;
 	}
 	else{
-	tmp->next=a;
-	a=tmp;	
+		tmp->next=a;
+		a=tmp;	
 	}
 }
-void InsertLast(node &a, int x){
-	node tmp=makeNode (x);
-	if(a==NULL){
-		a=tmp;
-	}
-	else {
-		node p=a;
-		while(p->next != NULL){
-			p=p->next;
+//delete all cac gia tri bang x                                                                                
+void DeleteData(node &a, int x){
+	for(node p=a; p != NULL; p=p->next){
+		for(node q=p->next; q != NULL; q=q->next){
+			if(q->data ==x){
+				p->next=q->next;	
+			}
 		}
-		p->next=tmp;
 	}
 }
-//void DeleteFirst(node &a){
-//	if(a==NULL){
-//		return;
-//	}
-//	a=a->next;
-//}
-//void DeleteLast(node &a){
-//	if(a==NULL){
-//		return;
-//	}
-//	node truoc=NULL, sau=a;
-//	while(a->next != NULL){	
-//		truoc=sau;
-//		sau=sau->next;
-//	}
-//	if(truoc==NULL){
-//		a=NULL;
-//	}
-//	else{
-//		truoc->next=NULL;
-//	}
-//}
-void Delete(node &a, int pos){
-	int n=Size(a);
-	if(pos <=0 || pos > n+1){
-		return;
-	}
-//	if(pos == 1){
-//		DeleteFirst(a); return;
-//	}
-//	if(pos == n+1){
-//		DeleteLast(a); return;
-//	}
-	node truoc=NULL, sau=a;
-	for(int i=1; i<pos; i++){
-		truoc=sau;
-		sau=sau->next;
-	}
-	if(truoc==NULL){
-		a=a->next;
-	}
-	else{
-		truoc->next=sau->next;
-	}
-}
-ll Sum (node a){
-	ll s=0;
+// hien thi danh sach tren 1 hang
+void ShowList(node a){
 	node p=a;
-	while(p != NULL){
-		s+=p->data;
+	while(p !=NULL){
+		cout << p->data << " ";
 		p=p->next;
 	}
-	return s;
+	cout <<  endl;
+}
+//nghich dao danh sach lien ket danh sach lien ket
+void Reveser(node &a){
+	node prev=NULL;
+	node cur=a;
+	node next=NULL;
+	while(cur != NULL){
+		next=cur->next;
+		cur->next=prev;
+		prev=cur;
+		cur=next;
+	}
+	a=prev;
+}
+//sap xep theo thu tu tang dan
+void Sort(node a){
+	for(node p=a;p != NULL;p=p->next){
+		for(node q=p->next; q != NULL; q=q->next){
+			if(q->data < p->data) {
+				swap(q->data, p->data);
+			}
+		}
+	}
+}
+// xoa tat ca cac gia  tri trung nhau
+void DeleteAll(node &a){
+	node truoc=a;
+	node sau=a->next;
+	while(sau != NULL){
+		if(truoc->data == sau->data){
+			truoc->next = sau->next;
+		}
+		truoc=sau;
+		sau=sau->next;
+	}	
+}
+int  Count(node a){
+	int cnt=0;
+	node p=a;
+	while(p != NULL){
+		cnt++;
+		p=p->next;
+	}
+	return cnt;
+}
+void CreateList(node &a,string s){
+	stringstream ss(s);
+	string tmp;
+	while(ss >>tmp){
+		int x=stoi(s);
+		InsertHead(a, x);
+	}
 }
 int main() {
 	node head=NULL;
-	int n; cin >> n;
-	for(int  i=1; i<=n;i++){
-		int x; cin >> x;
-		InsertLast(head, x);
-	}
 	string s;
 	while(cin >> s){
-		int x; int pos;
-		if(s[0]=='i'){
+		int x;
+		if(s=="ins"){
 			cin >> x;
-			InsertLast(head, x);
+			InsertHead(head, x);
+			Sort(head);
 		}
-		else if(s[0]=='d'){
-			cin >> pos;
-			Delete(head, pos);
+		else if(s=="del"){
+			cin >> x;
+			DeleteData(head, x);
 		}
-		else if(s[0]=='s'){
-			cout << Sum(head) << endl;
+		else if(s=="dir"){
+			ShowList(head);
 		}
-		else if(s[0]=='0'){
-			int tmp; cin>> tmp;
-			if(tmp==0) break;
+		else if(s=="rvs"){
+			Reveser(head);
 		}
-	}
+		else if(s=="ord"){
+			Sort(head);
+		}
+		else if(s=="dup"){
+			DeleteAll(head);
+		}
+		else if(s=="cnt"){
+			cout << Count(head) << endl;
+		}
+		else if(s=="end"){
+			break;
+		}
+		else {
+			stringstream ss(s);
+			int tmp;
+			ss >> tmp;
+			InsertHead(head, tmp);
+		}
+	}	
 	return 0;
 }
 /*
-6
-1 1 1 1 1 1
-i 4
-s
-i 6
-i 12
-i 1
-s
-d 1
-s
-list 3.9
+1 2 4 5 3 6 4 8 9 2 5 6
+dir
+rvs
+dir
+ord
+dir
+del 2
+dir
+ins 2
+dup
+dir
+cnt
 */
